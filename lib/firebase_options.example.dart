d8595e35326e5ef970acfe36d0457677f4206334
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-05-14 10:48:13 +0530

    lib/firebase_options.example.dart

---
+++ lib/firebase_options.example.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => web;
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID',
+    measurementId: 'YOUR_MEASUREMENT_ID',
+  );
+}
